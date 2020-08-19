db8a8a1fe8c431108946691d4429442b85152b7d
onlinejke <barnybarny93@gmail.com>
2020-08-20 01:37:33 +0300

    second

---
+++ 2.py
@@ -0,0 +1,7 @@
+import urllib.request
+url="https://royallib.com/book/pelevin_viktor/chapaev_i_pustota.html"
+with urllib.request.urlopen(url) as webpage:
+    for line in webpage:
+    line = line.strip()
+    line = line.decode('utf-8') # преобразуем тип bytes в utf-8
+    print(line)    
